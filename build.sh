08b65c9d92dc256665c1281a879f1ba5ad9f1493
Riccardo Di Bella <riccardodibella2001@gmail.com>
2025-03-29 20:32:19 +0100

    09 created

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # https://chatgpt.com/share/67e8062d-fd34-8007-8492-0853ae27c423
-gcc 08.c -DMAIN_MODE=CLIENT -o client.out
+gcc 09.c -DMAIN_MODE=CLIENT -o client.out
 if [[ "$1" == "--local" ]]; then
-    gcc 08.c -DMAIN_MODE=SERVER -DLOCAL_SERVER -o server.out
+    gcc 09.c -DMAIN_MODE=SERVER -DLOCAL_SERVER -o server.out
 else
-    scp 08.c dibella@liquigas.duckdns.org:/home/dibella
-    ssh dibella@liquigas.duckdns.org 'gcc 08.c -DMAIN_MODE=SERVER -o server.out'
+    scp 09.c dibella@liquigas.duckdns.org:/home/dibella
+    ssh dibella@liquigas.duckdns.org 'gcc 09.c -DMAIN_MODE=SERVER -o server.out'
 fi
