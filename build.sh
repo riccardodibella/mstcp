9be2911a6385a6da6480ae9355049aeee50b883a
Riccardo Di Bella <riccardodibella2001@gmail.com>
2025-06-04 20:56:03 +0200

    10 created

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "$(date '+%d/%m/%Y %H:%M:%S')"
 # https://chatgpt.com/share/67e8062d-fd34-8007-8492-0853ae27c423
-gcc 09.c -DMAIN_MODE=CLIENT -o client.out
+gcc 10.c -DMAIN_MODE=CLIENT -o client.out
 if [[ "$1" == "--local" ]]; then
-    gcc 09.c -DMAIN_MODE=SERVER -DLOCAL_SERVER -o server.out
+    gcc 10.c -DMAIN_MODE=SERVER -DLOCAL_SERVER -o server.out
 else
-    scp 09.c dibella@liquigas.duckdns.org:/home/dibella
-    ssh dibella@liquigas.duckdns.org 'gcc 09.c -DMAIN_MODE=SERVER -o server.out'
+    scp 10.c dibella@liquigas.duckdns.org:/home/dibella
+    ssh dibella@liquigas.duckdns.org 'gcc 10.c -DMAIN_MODE=SERVER -o server.out'
 fi
