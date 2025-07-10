f26eac00554f201e0b8a6a80f2c12c2a889ee047
Riccardo Di Bella <riccardodibella2001@gmail.com>
2025-07-10 22:24:06 +0200

    12 created

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "$(date '+%d/%m/%Y %H:%M:%S')"
 # https://chatgpt.com/share/67e8062d-fd34-8007-8492-0853ae27c423
-gcc 11.c -DMAIN_MODE=CLIENT -o client.out
+gcc 12.c -DMAIN_MODE=CLIENT -o client.out
 if [[ "$1" == "--local" ]]; then
-    gcc 11.c -g -DMAIN_MODE=SERVER -DLOCAL_SERVER -o server.out
+    gcc 12.c -g -DMAIN_MODE=SERVER -DLOCAL_SERVER -o server.out
 else
-    scp 11.c dibella@172.104.237.69:/home/dibella
-    ssh dibella@172.104.237.69 'gcc 11.c -g -DMAIN_MODE=SERVER -o server.out'
+    scp 12.c dibella@172.104.237.69:/home/dibella
+    ssh dibella@172.104.237.69 'gcc 12.c -g -DMAIN_MODE=SERVER -o server.out'
 fi
