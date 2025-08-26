ed7d90e784d540ccfd967c7b2602158a7c9ea3b1
Riccardo Di Bella <riccardodibella2001@gmail.com>
2025-08-26 10:04:38 +0200

    15 created

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 echo "$(date '+%d/%m/%Y %H:%M:%S')"
 # https://chatgpt.com/share/67e8062d-fd34-8007-8492-0853ae27c423
 if [[ "$1" == "--local" ]]; then
-    gcc 14.c -O3 -g -DMAIN_MODE=CLIENT -DLOCAL_SERVER -o client.out
+    gcc 15.c -O3 -g -DMAIN_MODE=CLIENT -DLOCAL_SERVER -o client.out
 else
-    gcc 14.c -O3 -g -DMAIN_MODE=CLIENT -o client.out
+    gcc 15.c -O3 -g -DMAIN_MODE=CLIENT -o client.out
 fi
 # https://chatgpt.com/share/68971012-0f38-8007-afae-7fdf7c85a6a2
 if [[ $? -ne 0 ]]; then
@@ -12,8 +12,8 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 if [[ "$1" == "--local" ]]; then
-    gcc 14.c -O3 -g -DMAIN_MODE=SERVER -DLOCAL_SERVER -o server.out
+    gcc 15.c -O3 -g -DMAIN_MODE=SERVER -DLOCAL_SERVER -o server.out
 else
-    scp 14.c dibella@172.104.237.69:/home/dibella
-    ssh dibella@172.104.237.69 'gcc 14.c -O3 -g -DMAIN_MODE=SERVER -o server.out'
+    scp 15.c dibella@172.104.237.69:/home/dibella
+    ssh dibella@172.104.237.69 'gcc 15.c -O3 -g -DMAIN_MODE=SERVER -o server.out'
 fi
