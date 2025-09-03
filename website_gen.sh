19e3fc726d40548cfd2f49555b6166fe858b4a5e
Riccardo Di Bella <riccardodibella2001@gmail.com>
2025-09-03 22:00:13 +0200

    15 progressive jpeg truncation

--- website_gen.sh
+++ website_gen.sh
@@ -2,23 +2,46 @@
 rm -r website_folder
 mkdir website_folder
 
-curl -L -o website_folder/favicon.png "https://picsum.photos/64"
 
 # number of images (you can pass as argument, e.g. ./script.sh 5)
 n=${1:-3}
+# number of bytes to keep for each image (optional second parameter)
+truncate_bytes=${2:-0}
+
+img_side=640
+
+if [ "$truncate_bytes" -gt 0 ]; then
+    curl -L -o website_folder/favicon.jpg "https://picsum.photos/$img_side"
+    convert website_folder/favicon.jpg -interlace JPEG website_folder/favicon.jpg
+    head -c $truncate_bytes website_folder/favicon.jpg > website_folder/tempfavicon.jpg
+    mv website_folder/tempfavicon.jpg website_folder/favicon.jpg
+else
+    curl -L -o website_folder/favicon.jpg "https://picsum.photos/64"
+fi
+
 
 cat << EOF > website_folder/index.html
 <!DOCTYPE html>
 <html>
 <head>
 <title>Test Page</title>
+<link rel="icon" type="image/jpeg" href="favicon.jpg">
+<style>
+body { display: flex; flex-wrap: wrap; }
+img {width: 25%; padding: 2px; box-sizing: border-box;}
+</style>
 </head>
 <body>
 EOF
 
 for i in $(seq 1 $n); do
-  curl -L -o website_folder/image$i.png "https://picsum.photos/400"
-  echo "<img src=\"image$i.png\">" >> website_folder/index.html
+    curl -L -o website_folder/image$i.jpg "https://picsum.photos/$img_side"
+    if [ "$truncate_bytes" -gt 0 ]; then
+        convert website_folder/image$i.jpg -interlace JPEG website_folder/image$i.jpg
+        head -c $truncate_bytes website_folder/image$i.jpg > website_folder/temp$i.jpg
+        mv website_folder/temp$i.jpg website_folder/image$i.jpg
+    fi
+    echo "<img src=\"image$i.jpg\">" >> website_folder/index.html
 done
 
 cat << EOF >> website_folder/index.html
