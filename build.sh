41eaac21266e1ebab5c0306f07346b084a473098
Riccardo Di Bella <riccardodibella2001@gmail.com>
2025-10-16 10:06:42 +0200

    16 created

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 echo "$(date '+%d/%m/%Y %H:%M:%S')"
 # https://chatgpt.com/share/67e8062d-fd34-8007-8492-0853ae27c423
 if [[ "$1" == "--local" ]]; then
-    gcc 15.c -O3 -g -DMAIN_MODE=CLIENT -DLOCAL_SERVER -o client.out
+    gcc 16.c -O3 -g -DMAIN_MODE=CLIENT -DLOCAL_SERVER -o client.out
 else
     if [[ "$1" == "--ms_test" ]]; then
-    gcc 15.c -O3 -g -DMAIN_MODE=CLIENT -DMS_ENABLED=true -DNO_DEBUG -o client_ms.out
-    gcc 15.c -O3 -g -DMAIN_MODE=CLIENT -DMS_ENABLED=false -DNO_DEBUG -o client_tcp.out
+    gcc 16.c -O3 -g -DMAIN_MODE=CLIENT -DMS_ENABLED=true -DNO_DEBUG -o client_ms.out
+    gcc 16.c -O3 -g -DMAIN_MODE=CLIENT -DMS_ENABLED=false -DNO_DEBUG -o client_tcp.out
     else
-    gcc 15.c -O3 -g -DMAIN_MODE=CLIENT -o client.out
+    gcc 16.c -O3 -g -DMAIN_MODE=CLIENT -o client.out
     fi
 fi
 # https://chatgpt.com/share/68971012-0f38-8007-afae-7fdf7c85a6a2
@@ -17,8 +17,8 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 if [[ "$1" == "--local" ]]; then
-    gcc 15.c -O3 -g -DMAIN_MODE=SERVER -DLOCAL_SERVER -o server.out
+    gcc 16.c -O3 -g -DMAIN_MODE=SERVER -DLOCAL_SERVER -o server.out
 else
-    scp 15.c dibella@172.104.237.69:/home/dibella
-    ssh dibella@172.104.237.69 'gcc 15.c -O3 -g -DMAIN_MODE=SERVER -o server.out'
+    scp 16.c dibella@172.104.237.69:/home/dibella
+    ssh dibella@172.104.237.69 'gcc 16.c -O3 -g -DMAIN_MODE=SERVER -o server.out'
 fi
